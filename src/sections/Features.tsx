f7c1357d779f8492bb1c03a9b219eb7084356471
harsh6183 <harshnimbalkar6181@gmail.com>
2025-08-03 21:20:43 +0530

    Done with Landing page

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -26,10 +26,10 @@ export default function Features() {
                 <div className="flex justify-center">
                     <Tag>Features</Tag>
                 </div>
-                <h2 className="text-6xl font-medium text-center mt-6">Where power meets <span className="text-lime-400">simplicity</span></h2>
-                <div className="mt-12 grid grid-cols-1 md:grid-cols-4 gap-8">
+                <h2 className="text-6xl font-medium text-center mt-6 max-w-2xl mx-auto">Where power meets <span className="text-lime-400">simplicity</span></h2>
+                <div className="mt-12 grid grid-cols-1 md:grid-cols-4 lg:grid-cols-3 gap-8">
                     <FeatureCard title="Real-time Collaboration" description="Work together seamlessly with conflict-free team editing"
-                    className="col-span-2"
+                    className="md:col-span-2 lg:col-span-1"
                     >
                         <div className="aspect-videp flex items-center justify-center">
                             <Avatar className="z-40">
@@ -52,14 +52,14 @@ export default function Features() {
                         </div>
                     </FeatureCard>
                     <FeatureCard title="Interactive Prototyping" description="Engage your clients with prototypes that react to user actions"
-                    className="col-span-2"
+                    className="md:col-span-2  lg:col-span-1"
                     >
                         <div className="aspect-videp flex items-center justify-center">
                             <p className="text-4xl font-extrabold text-white/20 text-center">We have achieved <span className="bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent">incredible</span>{" "}growth this year</p>
                         </div>
                     </FeatureCard>
                     <FeatureCard title="Keyboard Quick Actions" description="Powerful commands to help you create designs more quickly"
-                    className="col-span-2 col-start-2"
+                    className="md:col-span-2 md:col-start-2  lg:col-span-1 lg:col-start-auto"
                     >
                         <div className="aspect-video flex gap-4 items-center ">
                             <Key className="w-28">shift</Key>
@@ -70,9 +70,9 @@ export default function Features() {
                 </div>
                 <div className="mt-8 flex flex-wrap gap-3 justify-center">
                     {features.map(feature => (
-                        <div key={feature} className="bg-neutral-900 border border-white/10 inline-flex px-3 py-1.5 rounded-2xl gap-3 items-center">
+                        <div key={feature} className="bg-neutral-900 border border-white/10 inline-flex px-3 py-1.5 rounded-2xl gap-3 items-center  md:px-3 md:py-2" >
                             <span className="bg-lime-400 text-neutral-950 size-5 rounded-full inline-flex text-center items-center justify-center text-xl">&#10038;</span>
-                            <span className="font-medium">{feature}</span>
+                            <span className="font-medium md:text-lg">{feature}</span>
                         </div>
                     ))}
                 </div>

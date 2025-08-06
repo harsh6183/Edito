e835548906c81e82a7f46a97743fabe1250fa95b
harsh6183 <harshnimbalkar6181@gmail.com>
2025-08-06 21:30:25 +0530

    Changes in footer section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,7 @@
+import logoImage from "@/assets/images/logo.svg"
+import { a } from "framer-motion/client";
+import Image from "next/image";
+
 const footerLinks = [
     { href: "#", label: "Contact" },
     { href: "#", label: "Privacy Policy" },
@@ -5,5 +9,23 @@ const footerLinks = [
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <section className="py-16">
+            <div className="container">
+                <div className="flex flex-col md:flex-row items-center md:justify-between gap-6">
+                    <Image src={logoImage} alt="Layers logo">
+
+                    </Image>
+                    <div>
+                        <nav className="gap-6 flex">
+                            {footerLinks.map((link)=>
+                            (
+                                <a href={link.href} className="text-white/50 text-sm">{link.label}</a>
+                            ))}
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
